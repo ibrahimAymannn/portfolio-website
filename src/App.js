be4ef81c8478e418023d3fa93aacec178411c1a6
ibrahimaymannn <20173565@fue.edu.eg>
2026-04-27 20:00:00 +0300

    contact&newsletter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import NavBar from './components/NavBar';
 import {Banner} from './components/Banner';
 import { Skills} from './components/Skills';
 import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
+import { Newsletter } from './components/Newsletter';
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,8 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
+      <Newsletter />
     </div>
   );
 }
