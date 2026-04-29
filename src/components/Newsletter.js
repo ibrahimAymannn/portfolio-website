9a78c3a7ba436a2d0d134c45d4536f3895ad376d
ibrahimaymannn <20173565@fue.edu.eg>
2026-04-29 07:13:52 +0300

    fix newsletter

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,24 +1,12 @@
 import { useState, useEffect } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
 
-export const Newsletter = ({ status, message, onValidated }) => {
+export const Newsletter = () => {
   const [email, setEmail] = useState('');
 
-  useEffect(() => {
-    if (status === 'success') clearFields();
-  }, [status])
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    email &&
-    email.indexOf("@") > -1 &&
-    onValidated({
-      EMAIL: email
-    })
-  }
-
-  const clearFields = () => {
-    setEmail('');
   }
 
   return (
@@ -27,9 +15,6 @@ export const Newsletter = ({ status, message, onValidated }) => {
           <Row>
             <Col lg={12} md={6} xl={5}>
               <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
-              {status === 'sending' && <Alert>Sending...</Alert>}
-              {status === 'error' && <Alert variant="danger">{message}</Alert>}
-              {status === 'success' && <Alert variant="success">{message}</Alert>}
             </Col>
             <Col md={6} xl={7}>
               <form onSubmit={handleSubmit}>
